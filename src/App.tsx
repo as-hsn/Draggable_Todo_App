fb11dfe4683cae58dd2e45eaa905842b8f419f09
Hassan <hassanarif1471@gmail.com>
2025-02-04 20:41:07 +0500

    Correct App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -6,10 +6,13 @@ import TodoApp from './components/Todo';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import { auth } from './components/firebase';
+import { User } from "firebase/auth";
 import './App.css';
 
 function App() {
-  const [user, setUser] = useState<firebase.User | null>(null);
+  // const [user, setUser] = useState<firebase.User | null>(null);
+
+  const [user, setUser] = useState<User | null>(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
