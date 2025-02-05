450ad88558cf2635ae2e81182c7d2f02818b2db4
Hassan <hassanarif1471@gmail.com>
2025-02-05 23:32:59 +0500

    remove 8  characters validation in login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -23,9 +23,7 @@ function Login() {
     email: Yup.string()
       .email("Invalid email format")
       .required("Email is required"),
-    password: Yup.string()
-      .min(8, "Password must be at least 8 characters")
-      .required("Password is required"),
+    password: Yup.string().required("Password is required"),
   });
 
   const handleOnSubmit = async (values: FormValues) => {
